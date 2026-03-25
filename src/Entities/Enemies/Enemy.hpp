2378067d61788e6c8489d642addd3ac905401943
jrosado051 <jose.rosado53@upr.edu>
2026-03-24 18:51:22 -0700

    Phase 2 Scoring bug fix 3.2

--- src/Entities/Enemies/Enemy.hpp
+++ src/Entities/Enemies/Enemy.hpp
@@ -64,6 +64,8 @@ class Enemy {
                     }
 
                     if (p.second->health <= 0) {
+                        score +- 100;
+                        
                         Animation::animations.push_back(
                             Animation(p.second->position.first, p.second->position.second, 155, 0, 33, 33, 30, 30, 4, ImageManager::SpriteSheet)
                         );
